9c1dc3be67d657af4d11a3f105081368b4a477e3
Kundan <kundanss22@gmail.com>
2025-02-28 22:26:00 +0530

    added global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 
 const app = express();
 
-//Routes
-//HTTP methods: get post patch delete put
-app.get("/", (req, res, next) => {
-  res.json({ message: "welcome to api" });
+// Routes
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  // Example of throwing an error (uncomment to test)
+  const error = createHttpError(400, "Something went wrong");
+  throw error;
+
+  res.json({ message: "Welcome to API" });
+  // next(); // No need to call next() here unless passing control further
 });
+
+// Global error handler
+app.use(globalErrorHandler);
 
 export default app;
