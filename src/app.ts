e377414117f9b6f968813674483abed757376a32
Kundan <kundanss22@gmail.com>
2025-03-03 10:53:24 +0530

    remove errot test from "/" route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express, { NextFunction, Request, Response } from "express";
-import createHttpError from "http-errors";
+// import createHttpError from "http-errors";
 import globalErrorHandler from "./middleware/globalErrorHandler";
 import userRouter from "./users/userRouter";
 import booksRouter from "./books/booksRouter";
@@ -9,8 +9,8 @@ app.use(express.json());
 // Routes
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   // Example of throwing an error (uncomment to test)
-  const error = createHttpError(400, "Something went wrong");
-  throw error;
+  // const error = createHttpError(400, "Something went wrong");
+  // throw error;
 
   res.json({ message: "Welcome to API" });
   // next(); // No need to call next() here unless passing control further
